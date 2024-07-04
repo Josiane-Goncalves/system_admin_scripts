cd2de8ca408d7c1fb5c7baf42c738d157de7c1ab
Josiane <josypropy@gmail.com>
2024-07-03 22:06:52 -0300

    Criando variáveis simples.

---
+++ welcome.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Solicitar o nome do usuário
+echo "Por favor, insira seu nome: "
+read nome
+
+# Solicitar a idade do usuário
+echo "Por favor, insira sua idade: "
+read idade
+
+# Solicitar a cidade do usuário
+echo "Por favor, insira sua cidade: "
+read cidade
+
+# Imprimir a mensagem de boas-vindas
+echo "Bem-vindo, $nome!"
+echo "Você tem $idade anos e mora em $cidade."
+echo "Esperamos que você tenha um ótimo dia!"
